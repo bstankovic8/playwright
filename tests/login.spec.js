6610a0cecfc39bd60745b26525a42e05c4bf9397
Bora <bstankovic@knowit.rs>
2025-02-12 11:08:34 +0100

    New simple tests(by tutorial)

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -8,10 +8,8 @@ test('Login test @smoke @regresion', async ({page}) => {
     await page.getByRole('button', { name: /login/i }).click();
     await expect(page.locator('#flash')).toContainText(/You logged into a secure area!/);
     await expect(page.locator('h4.subheader')).toContainText('Welcome to the Secure Area. When you are done click logout below.');
-    await page.pause();
     await page.locator('a.button.secondary.radius:has-text("Logout")').click();
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill('65');
     await expect(page.getByLabel('Username')).toHaveValue('65');
-    await page.pause();
 })
